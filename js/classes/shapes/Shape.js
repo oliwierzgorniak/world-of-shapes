6f3d43045ac4c2ee76c67851ce676b84d7427387
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2023-11-29 17:11:05 +0100

    shapes move

--- js/classes/shapes/Shape.js
+++ js/classes/shapes/Shape.js
@@ -8,10 +8,33 @@ export default class Shape {
       height * 0.01 * dashboard.size,
       height * 0.06 * dashboard.size
     );
+    this.vibrationOffset = { x: 0, y: 0 };
+    this.vibrateRandom = random(0, 100);
+  }
+
+  vibrate() {
+    const rangeEnd = width * 0.1;
+    const noiseParameter = millis() * 0.0001 + this.vibrateRandom;
+    this.vibrationOffset.x = map(
+      noise(noiseParameter),
+      0,
+      1,
+      -rangeEnd,
+      rangeEnd
+    );
+    this.vibrationOffset.y = map(
+      noise(noiseParameter + 1),
+      0,
+      1,
+      -rangeEnd,
+      rangeEnd
+    );
   }
 
   display() {
+    this.vibrate();
     push();
+    translate(this.vibrationOffset.x, this.vibrationOffset.y);
     translate(shapesOffset.x, shapesOffset.y);
     fill(this.color);
     this.draw();
